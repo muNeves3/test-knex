2d27af0e50c9ac1933e76222cfc645639aaef431
muNeves3 <murilogrilosn@gmail.com>
2020-09-09 15:26:57 -0300

    :gear: creating migrations, configuring the database and starting the server

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_test",
+      user: "postgres",
+      pasword: "0000"
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    }
+  }
+};
