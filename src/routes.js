b19c0c4ee5c54c56d445ab365c917eaf6682417d
muNeves3 <murilogrilosn@gmail.com>
2020-09-12 18:20:17 -0300

    :rocket: adding controllers and routes

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 const usercontroller = require('./controllers/usercontroller');
 
 routes.get('/users', usercontroller.index);
+routes.post('/users', usercontroller.create);
+routes.put('/users/:id', usercontroller.update);
+routes.delete('/users/:id', usercontroller.delete);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes');
+app.use(express.json());
 app.use(routes);
+
+//catch all - middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(3333, () => {
     console.log("O servidor está ligado");
