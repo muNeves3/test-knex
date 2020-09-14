6e04463f10a489beab767771479f50582a1adf36
muNeves3 <murilogrilosn@gmail.com>
2020-09-14 15:39:11 -0300

    :rocket: finalizando CRUD de usuarios

--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ const routes = require('./routes');
 app.use(express.json());
 app.use(routes);
 
+//not Found
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+})
+
 //catch all - middleware
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
